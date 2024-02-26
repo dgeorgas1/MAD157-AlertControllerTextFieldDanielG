6750ffdce10249abf6c62d8cd76bac61baa361af
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-02-25 23:21:30 -0600

    Commit changes

--- AlertControllerTextField/ViewController.swift
+++ AlertControllerTextField/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelResult.numberOfLines = 0
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Login", message: "Enter Password", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: {
+            (textField) in textField.placeholder = "Password here"
+            textField.isSecureTextEntry = true
+        })
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            action -> Void in
+            let savedText = alert.textFields![0] as UITextField
+            self.labelResult.text = savedText.text
+        })
+        
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
